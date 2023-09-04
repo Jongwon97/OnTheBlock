5ac380acdf9d730121f7062f86a61c58170ceefb
seojoonhee <seojoonhee10@gmail.com>
2023-09-04 17:18:07 +0900

    feat:notice entity, repositoryã 추가
    Former-commit-id: 38015e0552aa94896d6cc9dddf26ddffb3c38476

---
+++ Backend/src/main/java/com/ontheblock/www/notice/domain/MemberNotice.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0a48e487aac082a885972c83da587e8544cf29404e5a5c6b547154d127fbda2f
+size 958
---
+++ Backend/src/main/java/com/ontheblock/www/notice/repository/MemberNoticeRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aeaee2aa533255eab5374d32255534563ad81c5ec2143f94225173c52e59118d
+size 318
