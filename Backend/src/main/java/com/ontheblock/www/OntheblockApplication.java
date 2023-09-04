ca4bdf24ddf5d854a330534be296c119ddd185d5
jongseong <whd1707@gmail.com>
2023-09-04 17:36:38 +0900

    feat: video entity 개발

--- Backend/src/main/java/com/ontheblock/www/OntheblockApplication.java
+++ Backend/src/main/java/com/ontheblock/www/OntheblockApplication.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7706602c8ffb9ef2035bcf933249c6b6fcbeebb96a40ce93858a98931a93e786
-size 319
+oid sha256:6e47fa2f96735d61f7b487bdf2ef1f462f6163443796d80d85d3b30990abe863
+size 411
