a5b59db44b62483bc308b2183cd6a5f86cf19807
seojoonhee <seojoonhee10@gmail.com>
2023-09-05 10:40:48 +0900

    fix:id name 수정
    Former-commit-id: 16fe09ae9d4e15410e0c99edf651f679f4a14cf7

--- Backend/src/main/java/com/ontheblock/www/instrument/domain/Instrument.java
+++ Backend/src/main/java/com/ontheblock/www/instrument/domain/Instrument.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74cb4a102afebc8050d6ed945dfe3afd76945bf1892ae101cc4b638d145488d8
-size 670
+oid sha256:2fd9b391834ecf866e17595cce9d5e88399dd67becdd4dddbf115ff860c8154d
+size 660
