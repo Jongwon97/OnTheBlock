fb53461a2628672dc42b4ac0aaf62c038e5455d1
jongseong <whd1707@gmail.com>
2023-09-05 10:10:49 +0900

    feat: Entity init
    Former-commit-id: 0c3b9fe8fb5aff182bbf77a5562c188b3983f310

---
+++ Backend/src/main/java/com/ontheblock/www/comment/domain/Comment.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a7ab44c8066ce4a762b7633af35983bceef05e83c0a9876cb428a23c6312ee90
+size 629
---
+++ Backend/src/main/java/com/ontheblock/www/videowatch/domain/VideoWatch.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:58dd7a02d904bf1f78e602cb510ceca5c0ebee292a7d6fb1ada2f6a2fdf06107
+size 574
