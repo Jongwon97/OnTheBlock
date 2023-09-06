a6c8a3a5ca1b431cfc19763cf0bd6d15c52c2cb6
seojoonhee <seojoonhee10@gmail.com>
2023-09-06 14:57:15 +0900

    feat:membernotice service and controller
    Former-commit-id: 5aebd012321ffde8c12861e94dc9b19a99530ed8

--- Backend/src/main/java/com/ontheblock/www/notice/service/MemberNoticeService.java
+++ Backend/src/main/java/com/ontheblock/www/notice/service/MemberNoticeService.java
@@ -1,6 +1,3 @@
-package com.ontheblock.www.notice.service;
-
-public class MemberNoticeService {
-
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:a84581f0f8f32f83e50f140925e3131ff394e6dbaabee6ba1eaedba9f7340998
+size 2088
