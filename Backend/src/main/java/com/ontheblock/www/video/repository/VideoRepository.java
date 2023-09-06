2ff21c69dcde5ea3f13b29b22fe1c84c3c12bf5b
jongseong <whd1707@gmail.com>
2023-09-06 16:51:46 +0900

    refactor: 오타 수정
    Former-commit-id: 8291e5d978311d214dd4d69de400f0632939acff

--- Backend/src/main/java/com/ontheblock/www/video/repository/VideoRepository.java
+++ Backend/src/main/java/com/ontheblock/www/video/repository/VideoRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73cad5ecb10863f73b6d0d55a30d659d7bc5631e4c8fc09df62ae198e5c24b4b
-size 590
+oid sha256:d1439ea426e28e48010c683e1c0251cf50733aee2713f1c732946699a9f9d4c5
+size 714
