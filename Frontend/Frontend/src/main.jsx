8d374224b3757738020b12093b2066ae3f78a848
seojoonhee <seojoonhee10@gmail.com>
2023-09-07 14:48:15 +0900

    feat:mypage 페이지 생성
    Former-commit-id: 4dbc9eb54c39675cc03229f49c7532b6fc374de4

---
+++ Frontend/Frontend/src/main.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:acb24feb3540b5ee687f5a65bdbb81fe3644aa19da89256bc24b18d8ee010410
+size 235
---
+++ Frontend/src/pages/mypage/MyPage.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1c3a494e3ec88250ddf83170fab8626b6c6c9530b55b93ceba7eff6ebeb7dcb0
+size 1131
