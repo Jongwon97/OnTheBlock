e9d78f257839ebe0b1ce10197a8ff35ac9b1208d
seojoonhee <seojoonhee10@gmail.com>
2023-09-07 14:52:18 +0900

    del: 잘못 들어온 폴더 삭제
    Former-commit-id: ab1cdbfbfc1615f46d67a8488236bddcaf36d65a

--- Frontend/Frontend/.eslintrc.cjs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1ac9f863fde28829d2c38a4718ceee94df550d6f574a65d2e0559b113a4b1119
-size 526
--- Frontend/Frontend/README.md
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:22ac249d3674c2fe6663bb77aafd74502a0103b7fac45ef395dfd09974da7561
-size 451
