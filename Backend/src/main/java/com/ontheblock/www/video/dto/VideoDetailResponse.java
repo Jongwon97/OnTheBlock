c11cb766b1fbe59ee2331cb82cfa256f0f5fb12a
jongseong <whd1707@gmail.com>
2023-09-07 12:28:13 +0900

    feat: video controller 개발
    Former-commit-id: 2d22fa0c24cb0da1cb7d048a19a840d81c1d3af6

--- Backend/src/main/java/com/ontheblock/www/video/dto/VideoDetailResponse.java
+++ Backend/src/main/java/com/ontheblock/www/video/dto/VideoDetailResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b443cb680d4d0fceefb3bc558ed317ab2633c4155f5c02fdccb2dc940d8389d
-size 675
+oid sha256:b96d4f69307b88d3df2ed5e42e9e356e3122a75fbb643fe89d428f28c6e986d8
+size 1033
--- Backend/src/main/java/com/ontheblock/www/video/service/VideoService.java
+++ Backend/src/main/java/com/ontheblock/www/video/service/VideoService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8638125b98f44eb14cfcd3ceaba0d32bc042456069342da15af2ebad041718fc
-size 5522
+oid sha256:727eef4d0fa12c368f98cbd1d0bb3e480799702b8bedc349fd4e783ccf838226
+size 5596
