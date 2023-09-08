3b819321f7e9b69b84eafbfce99971ca022536b4
seojoonhee <seojoonhee10@gmail.com>
2023-09-08 10:33:03 +0900

    fix:mypage, profile naming 수정
    Former-commit-id: a309d0b31fd0376585ac016b5ca1aebd99e51d91

--- Frontend/src/components/profile/ProfileInfo.jsx
+++ Frontend/src/components/profile/ProfileInfo.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec285facdff26ae609ee223740f405852efad73c7e70d9746584df19192ca362
-size 1684
+oid sha256:d6930a269fff2e70a5ef20f5a5cd882d779ff7ea2ab5c692ba482f7608cc34a6
+size 1681
