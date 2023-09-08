663ceb2114e83b3edba2ec2c96dcc4dd8cfddd99
gyqls234 <gyqls234@naver.com>
2023-09-08 18:03:32 +0900

    feat: record + composition on progress +1
    Former-commit-id: 3b66fd38443b452fd283219cc401c65428c39731

--- Frontend/src/components/record/RecordComponent.jsx
+++ Frontend/src/components/record/RecordComponent.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2be8686927a42456f34680cdbc4cc795b0ba36a61c3d1efe38db242fed1e5aa8
-size 2736
+oid sha256:f7aeb3222cd8b7c8496a188a5e122c24d56ab290b9e621404dcdbe627a949b04
+size 3273
