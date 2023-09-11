3d23d6a52a498d7439de0824c58e41f9a4ef36a3
seojoonhee <seojoonhee10@gmail.com>
2023-09-11 13:49:00 +0900

    feat: mypageinformation 페이지 양식 추가
    Former-commit-id: 5dd2fc7ca003279c80800432fd9d7536a9e5dc7d

--- Frontend/src/components/mypage/MyPageInfo.jsx
+++ Frontend/src/components/mypage/MyPageInfo.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57cc2192eabdff3e53e382a5ab3db1b1f3b509dbf93ae32ba6420498efa58384
-size 3918
+oid sha256:3c3a4f445b37c10748722a83f8805350ca86ea1e41173a7e97f11b75bf942a70
+size 3916
--- Frontend/src/components/mypage/MyPageInfoInput.jsx
+++ Frontend/src/components/mypage/MyPageInfoInput.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca74b0a9da009a3b20089cb0f78f87e4ea1b56f874f08c16ed61191614ade16a
-size 2420
+oid sha256:739610ca13151e7bb48c0858861647ec703a322a9f543a9bf3a01ea40bdb88b0
+size 4276
