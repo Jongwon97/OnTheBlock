dece2033fa39f9904e7bd50dcd6f6c10932d5e24
seojoonhee <seojoonhee10@gmail.com>
2023-09-12 11:02:06 +0900

    fix: mypage, profile에서 thumbnail 표출 수정
    Former-commit-id: 18bc3547ad53dcd1ccd3f7af336e8d31be0b2670

--- Frontend/src/components/mypage/MyPageContent.jsx
+++ Frontend/src/components/mypage/MyPageContent.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84b1160c30b172b7ceaf49cdc10633f64ccf0ad4a19aeabfadf1f1aa55df052b
-size 2397
+oid sha256:42c1a21a8f426d0ea29ac30e44fb4eeb8b1fb59b5458af17d7a127902074c390
+size 2604
--- Frontend/src/components/profile/ProfileContent.jsx
+++ Frontend/src/components/profile/ProfileContent.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32e20a7c8fdd270862ec3f053e74e07707ee5d5e92ff43b39bd40e8fd3f4c56d
-size 1429
+oid sha256:3c316f5ec5751b0b4ac9ef24e029ba95f6515666392459d766c9970df1e422f9
+size 1442
