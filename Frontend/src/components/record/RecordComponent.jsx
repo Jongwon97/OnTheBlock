adb6d3ca8cf3b7def821689eb7ecf94439d0fe69
gyqls234 <gyqls234@naver.com>
2023-09-13 16:08:21 +0900

    feature: basic ui complete
    Former-commit-id: 6987f9a4c861c9354ae89bb185d2ef76c68a40fa

--- Frontend/src/components/record/RecordComponent.jsx
+++ Frontend/src/components/record/RecordComponent.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bac955f96263c8c8bcd656d4faf16cc721a53bba90d5d5a67ca56aadd70f42d6
-size 5966
+oid sha256:e8c65c5d8ea73df47fffe2f7cd4fb025f832e6903f484fe30fcf9a253d3dc9e2
+size 8562
