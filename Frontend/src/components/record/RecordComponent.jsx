2520234ce9f389114932610c2e56f03ce226db4c
gyqls234 <gyqls234@naver.com>
2023-09-15 15:14:29 +0900

    feature: merged develop changes
    Former-commit-id: 9cb260ef329b6892cda155df79d48e149a48b93a

--- Frontend/src/components/record/RecordComponent.jsx
+++ Frontend/src/components/record/RecordComponent.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55578c557387ffda36cb86582afc6beb652ae6b0286eb824fd2300a0e5a8edfb
-size 7579
+oid sha256:73dabf5a83e93ccf5aa90eb6a748e63200d85c86fca772c6451e7d671410886d
+size 7651
