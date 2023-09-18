3bd88af365745b9850e7b8aa5dfba80f94688386
seojoonhee <seojoonhee10@gmail.com>
2023-09-18 16:43:26 +0900

    feat: follow following 요청응답
    Former-commit-id: 8aa03e60ba433bb0848d1afbdee0d6b32d5b64d9

--- Frontend/src/api/follow.js
+++ Frontend/src/api/follow.js
@@ -1,11 +1,3 @@
-import { client, clientWithToken } from "./client";
-
-const followURL = 'follow/';
-
-export const getFollower=()=>{
-    return clientWithToken().get(followURL + 'member/follower/check');
-}
-
-export const getFollowing=()=>{
-    return clientWithToken().get(followURL + 'member/following/check');
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:cb1e0adc8e5bc2bb25d2274975210666a6cd9e5d27d7a885c3b8a75064c135ff
+size 541
--- Frontend/src/components/profile/ProfileInfo.jsx
+++ Frontend/src/components/profile/ProfileInfo.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a6e3f1a60bfe2f30d4cecc43289b760d367790e609c6ccfae940cea33423dfb
-size 3457
+oid sha256:db45f1a151489a8f9a8810bd9648e17f8a448f8183ed6af021b9135d55cb3fac
+size 4082
