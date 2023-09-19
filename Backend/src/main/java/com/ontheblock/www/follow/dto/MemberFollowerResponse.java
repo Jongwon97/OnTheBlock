8d7057809948366661a7f9c12d1447f3d6e95e0a
seojoonhee <seojoonhee10@gmail.com>
2023-09-19 10:51:27 +0900

    feat: follow following 프론트
    Former-commit-id: 796fc494e43456a3525658d92a464845802351ba

--- Backend/src/main/java/com/ontheblock/www/follow/dto/MemberFollowerResponse.java
+++ Backend/src/main/java/com/ontheblock/www/follow/dto/MemberFollowerResponse.java
@@ -1,3 +1,19 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d343f0fdd6a0f8e658ac805176e14a4e67d441f9a71ee93c15a0c4c99a583e4c
-size 492
+package com.ontheblock.www.follow.dto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import com.ontheblock.www.follow.domain.MemberFollow;
+
+import lombok.Data;
+
+@Data
+public class MemberFollowerResponse {
+
+	private List<MemberFollowDTO> followers;
+
+	public MemberFollowerResponse(List<MemberFollow> followers) {
+		this.followers = followers.stream()
+			.map(f -> new MemberFollowDTO(f.getFollower().getId(), f.getFollower().getNickName()))
+			.collect(Collectors.toList());
+	}
+}
