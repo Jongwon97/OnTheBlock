bdb137191a9f061008fa0d73ce67e4762d02c17d
seojoonhee <seojoonhee10@gmail.com>
2023-09-19 13:57:23 +0900

    feat: follow following 표출
    Former-commit-id: 54157130ff57ddda4f9e174cbf7b54a2bd823ceb

--- Frontend/src/components/mypage/MyPageInfo.jsx
+++ Frontend/src/components/mypage/MyPageInfo.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc5304959f1f1b8fbb8ae050576959857bdec96224945d6273db492c735cfadd
-size 4758
+oid sha256:8414a432c4ae4bbf1fdaa49400364f51653a25815ba1f6cff6928ae0205248f0
+size 4898
--- Frontend/src/components/profile/ProfileInfo.jsx
+++ Frontend/src/components/profile/ProfileInfo.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38ea5ea5ef5eed1ece620f341788e1d50eb666fdf45bad4a028acca1f6a51f25
-size 4649
+oid sha256:7dc67de9a1b87409e529aa3fa9ef422db90cdd668fa30498ab0009b59becf88e
+size 4753
