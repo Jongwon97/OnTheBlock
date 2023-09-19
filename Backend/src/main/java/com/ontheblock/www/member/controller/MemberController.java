ffe2a3d25b6b0a3437fdaf51452e158bf7f38170
seojoonhee <seojoonhee10@gmail.com>
2023-09-19 16:28:02 +0900

    feat: profile error page 추가
    Former-commit-id: d5ea688b42721d322b5e42f6cfb744e8ba0c0df0

--- Backend/src/main/java/com/ontheblock/www/member/controller/MemberController.java
+++ Backend/src/main/java/com/ontheblock/www/member/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2d16d39cbcaf897cdf438498c497258963a7fe2d2942385581d2e38138e7b1a
-size 1459
+oid sha256:f27b92d94c0d5771a41e8e37f4772668ac66523b0df9f3af07a8142029380aa9
+size 1462
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15fa81cfd2c982be3c81754a664de52109bb429d6eca728578b84d4e9fb3d395
-size 2275
+oid sha256:54914b10fec15c9ab77638e8bb14ebc3a8b6fc0c6850e29d95bd661762f18c3f
+size 2406
