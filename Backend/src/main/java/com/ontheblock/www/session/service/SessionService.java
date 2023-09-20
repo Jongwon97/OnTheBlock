1ada35e05e2e001c0ab26670bace513090dbe0ef
Jongwon97 <whddnjs3214@naver.com>
2023-09-20 17:51:15 +0900

    feat: backend/thumbnail/init
    Former-commit-id: d01235c72201fa0c86a8cd7e4f6e17554596eda4

--- Backend/src/main/java/com/ontheblock/www/session/service/SessionService.java
+++ Backend/src/main/java/com/ontheblock/www/session/service/SessionService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc50cc89d728b876b9fe40a0bff9dcdc679eb7a1eb46899da0c04ceb7d260956
-size 3408
+oid sha256:8a078ac81a17063d11da7aeed00b58efbd3029f67b3c13d8a2108e8a654a67ff
+size 3415
