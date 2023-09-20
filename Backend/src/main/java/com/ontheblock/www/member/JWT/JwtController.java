821dcc6294221a4fc8e983918c847c41ba3fe5ec
Jongwon97 <whddnjs3214@naver.com>
2023-09-20 14:58:38 +0900

    feat: Backend/Login/token/reissue
    Former-commit-id: 378ebc65dfdf56a3754343eff6fc445dfede2ffe

---
+++ Backend/src/main/java/com/ontheblock/www/member/JWT/JwtController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9c19da81cb9577f1e9da52b8017463d0a9e9d9fa5d13c7e22784e6207973ec65
+size 1933
--- Frontend/src/api/member.js
+++ Frontend/src/api/member.js
@@ -1,7 +1,3 @@
-import { client, clientWithToken } from "./client";
-
-const MemberURL = 'member/';
-
-export const getUserInfo = (memberId) => {
-    return clientWithToken().get(MemberURL + 'check', { params: { memberId } });
-  };
+version https://git-lfs.github.com/spec/v1
+oid sha256:bfe3a164f6a74b5b39ac17a4bfde4fd5c895fe828b8c8caf62e3e56d74a1ea60
+size 212
