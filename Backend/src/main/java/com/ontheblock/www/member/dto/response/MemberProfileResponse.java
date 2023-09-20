2db6f5f7c70439e4f3134ebef1c12de46c24a020
seojoonhee <seojoonhee10@gmail.com>
2023-09-20 16:32:51 +0900

    fix: myinfo nickname description
    Former-commit-id: 56ce5d8a8968997d923c7f48a41a6b18ac79e32c

--- Backend/src/main/java/com/ontheblock/www/member/dto/response/MemberProfileResponse.java
+++ Backend/src/main/java/com/ontheblock/www/member/dto/response/MemberProfileResponse.java
@@ -1,19 +1,3 @@
-package com.ontheblock.www.member.dto.response;
-
-import com.ontheblock.www.member.Member;
-
-import lombok.Data;
-
-@Data
-public class MemberProfileResponse {
-	private Long id;
-	private String nickName;
-	private String description;
-
-	// 프로필 정보
-	public MemberProfileResponse(Member member) {
-		this.id = member.getId();
-		this.nickName = member.getNickName();
-		this.description = member.getDescription();
-	}
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:d285b65689184ed70b52fee7ea6113a004d478499c0b97329baeacc458a4a456
+size 416
--- Frontend/src/api/member.js
+++ Frontend/src/api/member.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a8ba6cb52badda41671a0704dc1c1e3855912163659c94603df8bc28a9c5ba1
-size 585
+oid sha256:80759bbc7099c85c9f79dbbd7b5db2899a1d658ccec51609cec96c9008879407
+size 648
