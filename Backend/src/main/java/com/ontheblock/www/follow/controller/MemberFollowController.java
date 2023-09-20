b648c7ed46f545e1a4e37b5f41661d34d81e0a86
seojoonhee <seojoonhee10@gmail.com>
2023-09-20 10:57:40 +0900

    fix: follow following 본인 확인 부분 수정
    Former-commit-id: 9e5a53cc193bd7c5bba4495b0b2de2a79879def0

--- Backend/src/main/java/com/ontheblock/www/follow/controller/MemberFollowController.java
+++ Backend/src/main/java/com/ontheblock/www/follow/controller/MemberFollowController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a113a15acf6f6dbad4085f817e0ec695cde7e842c54b9159258c8c473469f644
-size 3831
+oid sha256:fcb97a6f2d54f53bf860b98d843d2c4caf19bb4b0b3c9a7cfd90ddd9a50d8ad3
+size 3937
