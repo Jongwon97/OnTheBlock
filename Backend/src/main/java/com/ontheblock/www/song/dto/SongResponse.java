fa4fad4a35285d86c7d240e9818c145b8549a496
jongseong <whd1707@gmail.com>
2023-09-20 14:27:56 +0900

    feat: song entity 구조 추가
    Former-commit-id: 1a2139036fc9e324b4fbf3285f990859af42d7fa

--- Backend/src/main/java/com/ontheblock/www/song/dto/SongResponse.java
+++ Backend/src/main/java/com/ontheblock/www/song/dto/SongResponse.java
@@ -1,10 +1,3 @@
-package com.ontheblock.www.song.dto;
-
-import lombok.Data;
-
-@Data
-public class SongResponse {
-    String name;
-    String code;
-    String artist;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:efcb4aead83f4ca8a06135eea62e6d397cf7f9ff7fe36b68108bdffb89013a0a
+size 148
