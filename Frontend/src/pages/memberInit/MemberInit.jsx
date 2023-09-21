5ebaccbb2d9fe2765b03f703c0ebf16423455ad1
seojoonhee <seojoonhee10@gmail.com>
2023-09-21 16:51:26 +0900

    feat: myinfoupdate instument genre
    Former-commit-id: 9581f4ce07bcd0ae3b1347f137f73630efabafcf

--- Frontend/src/pages/memberInit/MemberInit.jsx
+++ Frontend/src/pages/memberInit/MemberInit.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2cf19ce103d087fd269fbf10b90e6273e6920f6652565adffb8dd4e8f7b951d
-size 3382
+oid sha256:aa6ec219c466c59f8c39c04f93a1495339abf58cf0fd9e520e87aa9dbb3a5e7f
+size 3380
