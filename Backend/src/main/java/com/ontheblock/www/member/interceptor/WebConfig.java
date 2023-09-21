8156debfeb1dc0a3b4d88aa865f65455a141361f
Jongwon97 <whddnjs3214@naver.com>
2023-09-21 17:22:31 +0900

    faet: error
    Former-commit-id: b384ba0295892df4538960a0746c0cad704e8500

--- Backend/src/main/java/com/ontheblock/www/member/interceptor/WebConfig.java
+++ Backend/src/main/java/com/ontheblock/www/member/interceptor/WebConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1adcfcd58a1547510dc80e60d19a5e62db5f55e84d373e38b181a6f8333e45d2
-size 1302
+oid sha256:e3895de0a24c8e761718ca67e6986ae3f9240ac968e5f78e160a1a469eb5b060
+size 1298
