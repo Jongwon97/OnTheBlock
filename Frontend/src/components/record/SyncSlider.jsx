c7b39ee3fb84b9d59794e994efad2f9c455d68c1
gyqls234 <gyqls234@naver.com>
2023-09-22 10:34:10 +0900

    feat: basic sync ui created
    Former-commit-id: 97a8bbec572be0e712ef3e3844e6355778954600

--- Frontend/src/components/record/SyncSlider.jsx
+++ Frontend/src/components/record/SyncSlider.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16bb6cbcd10c141408c2dc6efe05d2f6806c33b6ce54c27f20c74ce0d81ea0d7
-size 1433
+oid sha256:052ee227f4c999c4689206b1cbb9df3e22ccbe2d81cec414c396fe809097c65e
+size 2557
