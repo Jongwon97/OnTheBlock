1bdc3a0290af2bf6086de04f53ee73585243a9a9
gyqls234 <gyqls234@naver.com>
2023-09-22 16:48:54 +0900

    fix : minor update
    Former-commit-id: 900bafbf7f7752e5f48e772316f2416651720020

--- Frontend/src/components/thumbnail/Thumbnail.jsx
+++ Frontend/src/components/thumbnail/Thumbnail.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27c710b8d3887f5032d32ff0d62d7e8c14bd4960e4073e7cdaec1e5487a6946b
-size 2558
+oid sha256:7419510d6ee57eacab6bb79cc04e2196c4419eecce5a956323d3d01727272dab
+size 2594
