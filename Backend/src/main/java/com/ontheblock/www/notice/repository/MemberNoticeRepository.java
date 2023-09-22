4a4bb51b192ca11207efa83b917e07aff38f1981
seojoonhee <seojoonhee10@gmail.com>
2023-09-22 16:47:19 +0900

    remove: import 삭제
    Former-commit-id: 04867f1bd9ed1738f99b26a42279d601e059ba90

--- Backend/src/main/java/com/ontheblock/www/notice/repository/MemberNoticeRepository.java
+++ Backend/src/main/java/com/ontheblock/www/notice/repository/MemberNoticeRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a23ff058c4d2ee2bd475113d610d40dc1c639ef9af9316fcf260b0efc71569ad
-size 652
+oid sha256:f076b0f76515558afffe06ca7bb61c15875f0e262d0fed86b46f4dbd5ecdee5b
+size 481
--- Backend/src/main/java/com/ontheblock/www/notice/service/MemberNoticeService.java
+++ Backend/src/main/java/com/ontheblock/www/notice/service/MemberNoticeService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9467e1d73bff93f2237d17cfda7edb5108cce08239d4c689e371cd6a63c871b5
-size 4397
+oid sha256:f7a27605f2e001b54dd9aac66fcca603c63a1f0d31f3fab26b57e4a131307943
+size 4224
