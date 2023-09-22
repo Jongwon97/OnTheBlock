d106d28193a6bc7665b563cacb68f3f046dff853
seojoonhee <seojoonhee10@gmail.com>
2023-09-22 13:16:37 +0900

    fix: droppanel css 수정
    Former-commit-id: c5b7c09661ab23c1cb951679cf9dedd369bb3454

--- Frontend/src/components/layout/UserDropPanel.jsx
+++ Frontend/src/components/layout/UserDropPanel.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c0613bd307ead9ea7fd517f5c06287c94ef8309ba0819b0a2f79e14e9bd4fd1
-size 2867
+oid sha256:ccb66504dd251ad4c17a8fe86ff5b58543f4bbddb3f0b1e8f66ecd644e41d07e
+size 3481
