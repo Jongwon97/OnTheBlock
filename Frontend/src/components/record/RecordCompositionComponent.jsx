3609f28e71c7f64e2cf73804f01da352fa5774cf
chb <gyqls234@gmail.com>
2023-09-23 20:44:23 +0900

    feat: minor refactor
    Former-commit-id: a02110a7b5476b9cca303fff7e255f054ee2c32e

--- Frontend/src/components/record/RecordCompositionComponent.jsx
+++ Frontend/src/components/record/RecordCompositionComponent.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bc5cc30522a51e83fdd480874778eff36a13de65e9bae4dab7e76849ce09eca
-size 17429
+oid sha256:b3ce937428ddf5498a3e0aa89cc75f349895090c7d2f558367cba735b92b3347
+size 13842
--- Frontend/src/components/record/RecordSessionComponent.jsx
+++ Frontend/src/components/record/RecordSessionComponent.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2379cc722e62ecda6cfcc8683069c74c7d926e71537e2a4a1075d564f2d19dea
-size 8603
+oid sha256:bcfb9a789d1cd17f81683156bbef56375e2212173bbf5041225cb574dd2fc207
+size 8522
