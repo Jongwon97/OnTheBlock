43e946375f2952a3832f16d734a59e4977aeb294
gyqls234 <gyqls234@naver.com>
2023-09-25 15:28:13 +0900

    fix: wrong image path corrected
    Former-commit-id: 3ddfd58d1a6dfa07b31ceeddaced9932b0017bdc

--- Frontend/src/components/video/VideoModal.jsx
+++ Frontend/src/components/video/VideoModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3f2ecba4288c82336104b122c21a9182fa10865edba870c3103f67f41afc8e0
-size 13111
+oid sha256:5154c0fb76718e102c8956361f6050e21370df0e9cca7754103255fcc0ac70c5
+size 12401
--- Frontend/src/pages/main/Main.jsx
+++ Frontend/src/pages/main/Main.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f64089092f3eb674b5f365fa92304876272498dcf04e7bb1b2403e36e87e9f38
-size 4929
+oid sha256:c61243cbd05aef358b55d5cc68fb8e863999af03e1aece75920d58d280108431
+size 4864
