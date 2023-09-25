0c40729db763b275a871eab1a3ec80efdba281b9
Jongwon97 <whddnjs3214@naver.com>
2023-09-25 13:57:12 +0900

    feat: comments update
    Former-commit-id: 570b8b18dc02cc3c9e56070b83f95a005d35484d

--- Backend/src/main/java/com/ontheblock/www/video/repository/VideoRepository.java
+++ Backend/src/main/java/com/ontheblock/www/video/repository/VideoRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d15c78bd5b9298d93267c3968179d7d658eef7222cb3d803c11b81da446a39f
-size 1982
+oid sha256:ed3a09e20bcc4bfbcc431510c50aecf463ee306c5b0a9f2c14f575b3dd300d29
+size 2010
