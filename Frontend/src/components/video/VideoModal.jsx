784eecd83d5cedf162e7df7ba1bc7d25d988f69a
gyqls234 <gyqls234@naver.com>
2023-09-25 11:23:44 +0900

    feat: comment delete from view
    Former-commit-id: 5c123747f8965815c4763cb50fd0e43d06f3fd66

--- Frontend/src/components/video/VideoModal.jsx
+++ Frontend/src/components/video/VideoModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7abbac4f11e1d8d466774b30f9289ab4ea16cb720d378c7e227b3f0ac9378fbd
-size 10167
+oid sha256:db0485fe41ec6d6817dc8bacbd2fd460a89940b19c3b126fc3bc9969c50eecf2
+size 10190
