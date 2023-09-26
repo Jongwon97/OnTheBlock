ca65fcc3b91059267fc9e878086423e4fbabb583
seojoonhee <seojoonhee10@gmail.com>
2023-09-26 17:56:06 +0900

    feat: 스크랩 알림 추가
    Former-commit-id: fca2b26bad4340b88d8d9356efb71fe6e0a8dbda

--- Backend/src/main/java/com/ontheblock/www/video/service/VideoService.java
+++ Backend/src/main/java/com/ontheblock/www/video/service/VideoService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc6398ee8f03eae797612aad057d0a894a3a80a93db4d745600c225cfd71de37
-size 8849
+oid sha256:061f89a9c7cebde1c4a041f6f972790f2a0de9d41daf6ce48ac120693809a478
+size 9261
