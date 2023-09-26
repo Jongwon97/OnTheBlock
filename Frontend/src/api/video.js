cde8453892fee70a1640589555f6ec24575d27e3
gyqls234 <gyqls234@naver.com>
2023-09-26 14:51:26 +0900

    feature: follow user video displayed on main page
    Former-commit-id: d5cb9418048022c44e85f39e95b7ffdf6844e838

--- Frontend/src/api/video.js
+++ Frontend/src/api/video.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a1144284862dd14f12b2efa4998da987b1536693989f04c1aee9bb0faf86292
-size 1952
+oid sha256:3e1c8623744a79c1d6c60531b9d7d1438f316fa230f3f2fd5f9b49a5c9035b51
+size 1950
