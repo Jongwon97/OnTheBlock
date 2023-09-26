cbd0a0204dfc1aaffeba6687628f9306cb8a0ee1
Jongwon97 <whddnjs3214@naver.com>
2023-09-26 10:17:38 +0900

    feat: Frontend/Video Api 추가
    Former-commit-id: dd6b0f981ca173a68ebf40882335770ff779d1b5

--- Frontend/src/api/video.js
+++ Frontend/src/api/video.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e98779e0d87f7b93a2468c3810f0747bb2484c8b248c6ac10a2df2e659fd5076
-size 1089
+oid sha256:76e25dffaf066e4d7a06bcbba352458f7d537d3c58419c3f0d992dab359ae1fd
+size 1586
