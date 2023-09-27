ba1df7e920233cee41bfcc0a9ab88963c1a6569e
seojoonhee <seojoonhee10@gmail.com>
2023-09-27 15:11:27 +0900

    feat: 글자수 제한
    Former-commit-id: c056f594c73735e377ad9867cf25bfacb7f87060

--- Frontend/src/pages/main/Main.jsx
+++ Frontend/src/pages/main/Main.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6dc141839ad0ed74d19888e409231c45dee735366d278276ce43019ad9e7a9f8
-size 6621
+oid sha256:9cd03c605d530631ea48e35dde2fd53498a4d8fbad52b13a36b78cb22b11ffba
+size 6652
