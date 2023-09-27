fdd626010a51e7d71b049a81d0ff9e7f85f8e54f
seojoonhee <seojoonhee10@gmail.com>
2023-09-27 10:55:56 +0900

    fix: 영상 댓글도 본인 알림 제외
    Former-commit-id: 3ec355cff7f6811801ebc45b20496b5b2b518a44

--- Backend/src/main/java/com/ontheblock/www/comment/service/CommentService.java
+++ Backend/src/main/java/com/ontheblock/www/comment/service/CommentService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64a21c6d37a94772a2702d9c4a1611e069b7e4b1d5a9c4d87afe6e825e0a287f
-size 3556
+oid sha256:df3a475e193e50e68696a1e15b0899d488e053e66955a848e39be55220af632a
+size 3655
