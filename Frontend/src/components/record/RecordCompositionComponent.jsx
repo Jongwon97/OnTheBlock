24df691399599f11c4c6a478a45199d8e682d3c4
이종원 <whddnjs3214@naver.com>
2023-09-30 21:30:07 +0900

    feat: Video cors
    Former-commit-id: 107a6167273d121200f5b6c837085b250ca99586

--- Frontend/src/components/record/RecordCompositionComponent.jsx
+++ Frontend/src/components/record/RecordCompositionComponent.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:533e798f952169881801a32124c7d27471af47d77a366ae7a9ff16480a1961e8
-size 17172
+oid sha256:f1889984ccdd8f1f8efea96904a0a21a1fd40cd94dc198b75dc07367c8ea3c08
+size 17038
