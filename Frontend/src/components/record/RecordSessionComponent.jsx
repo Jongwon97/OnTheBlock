01faadc2539102b8223631a72b97d85d99c98e3f
gyqls234 <gyqls234@naver.com>
2023-09-30 09:51:08 +0900

    design : session record form
    Former-commit-id: 54ff9a64da99f4164e91d17c76ea00a887e8aa74

--- Frontend/src/components/record/RecordSessionComponent.jsx
+++ Frontend/src/components/record/RecordSessionComponent.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea416eda6d7b2b57bfcaf8c367c2be48db07f596ea927edd920336d8507a11fa
-size 19344
+oid sha256:d6afdbb2fdac39273613558b592edfd07799406b266dfcfc105deaab54f6a139
+size 20677
---
+++ Frontend/src/components/record/RecordUploadForm.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8d75fd81162bc8bdbca737bc5ef0cb31b8db0ee0338cb46975c2ad78ac750bc4
+size 5623
