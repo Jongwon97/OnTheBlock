3d153866ef528a3fa6f19b969d069f51089e3326
gyqls234 <gyqls234@naver.com>
2023-10-01 11:04:22 +0900

    fix: commented suspicious elements
    Former-commit-id: 475e861ec72b1b7b7974599b40ad93bc2ef84eca

--- Frontend/src/components/gadgets/VolumeKnob.jsx
+++ Frontend/src/components/gadgets/VolumeKnob.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eff13569681b79fefbd492504f877d64dcbcf2fd61747c287093aa2a17e1bc70
-size 7255
+oid sha256:61ba209a12e02a53557ce14dc8dadfbb7c983a50c1ef784d6290cd7d91e21f54
+size 7257
