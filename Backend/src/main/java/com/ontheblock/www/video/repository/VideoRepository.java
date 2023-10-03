ba4a5b010f459496747df651c711f9d937f62720
박종성 <whd1707@gmail.com>
2023-10-03 15:36:47 +0900

    feat: 추천 서버에 접속 후 추천 리스트 가져오기 구현
    Former-commit-id: 7832a31a614e7b93ba565349c8c8b949694bd140

--- Backend/src/main/java/com/ontheblock/www/video/repository/VideoRepository.java
+++ Backend/src/main/java/com/ontheblock/www/video/repository/VideoRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2def2dc5a4f4884b30f3438dd0a4b9596728a7cc3ce35cc5dd6810f256ffc382
-size 3113
+oid sha256:5a9ea81dca69362c4c70b7a9f52d1bdb873eb0f8661607781f564fb9b8a7dd05
+size 3343
