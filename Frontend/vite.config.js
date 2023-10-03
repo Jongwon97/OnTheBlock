06dc54b34461e369eec4e49171a148bdab23ce82
gyqls234 <gyqls234@naver.com>
2023-10-03 12:29:31 +0900

    fix: build config
    Former-commit-id: 9a311aec5ffefd564c5864f924e9c79e8ed0786a

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,14 +1,3 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-
-export default defineConfig({
-  plugins: [react(), react],
-
-  resolve: {
-    alias: { 
-      '@' : path.resolve(__dirname, "./src")
-    }
-  },
-})
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:9e3362f103beea38511e4d8bbcf48f8e16f2ee8b7c5f16a949bb81968d5b8aaa
+size 560
