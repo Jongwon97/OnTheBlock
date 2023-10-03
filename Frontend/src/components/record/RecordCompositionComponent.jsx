82cbd7e7e6c292e70239e023d5fb2c7b567e9d29
gyqls234 <gyqls234@naver.com>
2023-10-04 08:53:51 +0900

    fix: song null
    Former-commit-id: 4b61ab55bd18067d2d6f6467c5cce7f8d13c0c93

--- Frontend/src/components/record/RecordCompositionComponent.jsx
+++ Frontend/src/components/record/RecordCompositionComponent.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:352b75c415d522743f0fbf0be2042b36236bc8e6b19acc63e955e29973b4d508
-size 27988
+oid sha256:004f4702a1f37b7ab508c9811ff4721e65ca7f8dbcfc24cf44cfd31c60a84dc7
+size 28014
