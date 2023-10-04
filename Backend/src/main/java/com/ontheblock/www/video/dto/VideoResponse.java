9c41f404437ababffa62a1eec8aa4848dc1627a4
jongseong <whd1707@gmail.com>
2023-10-04 09:53:20 +0900

    fix: 썸네일 url '+' 처리
    Former-commit-id: c07235df9020ebd79d94d32c0c0c33c45a1ea689

--- Backend/src/main/java/com/ontheblock/www/video/dto/VideoResponse.java
+++ Backend/src/main/java/com/ontheblock/www/video/dto/VideoResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13927cb4350a32e06a7c44bc3e9cdfcd730d91060c2a984be781ab7ea82ada24
-size 983
+oid sha256:736ccd815e1bbdd5d63f591978bc5671af0a4485cbaf571ba6830e754fe95e42
+size 1008
