1eea9ed5032c10a0fe83f819781c3f01113795be
gyqls234 <gyqls234@naver.com>
2023-10-04 10:12:42 +0900

    design: composition search redesign
    Former-commit-id: 9f30746d590abaea5cdb5ca8514e613633575991

--- Frontend/src/pages/main/Main.jsx
+++ Frontend/src/pages/main/Main.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31096a28e06b66f43bb712645dbb1d552c11a952d855293cd2119dae78a9b658
+oid sha256:0a04cd34076bfaa63a187bf808088690fd7daf2971ff83cff7b8a42ca85b1264
 size 6709
