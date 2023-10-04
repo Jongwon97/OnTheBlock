fdfc060efbdb0dd17e94ebfbefb26d2a26431f5a
seojoonhee <seojoonhee10@gmail.com>
2023-10-04 15:22:11 +0900

    feat: 알림 unRead 확인
    Former-commit-id: 66d7e1f981372c4df20d89f1410fbb491e0e9b63

--- Frontend/src/components/layout/Header.jsx
+++ Frontend/src/components/layout/Header.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99c4cb585fad7c9b49a6256e4f4a981a98e325819bc984ce2b72b96ffe0061e4
-size 2631
+oid sha256:3e204eeeb0cd62064a6c5bdcb94c6fa0d3336fdbd588b7d34c77b29247255a87
+size 2847
