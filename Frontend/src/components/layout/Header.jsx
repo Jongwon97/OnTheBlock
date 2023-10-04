075dcc537674bc67a8759519c23f8aa0ad227590
seojoonhee <seojoonhee10@gmail.com>
2023-10-04 09:59:05 +0900

    fix: 에러 관련 수정
    Former-commit-id: 8a8c926202ab04523e02d001d3160549ab943d2e

--- Frontend/src/components/layout/Header.jsx
+++ Frontend/src/components/layout/Header.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6dfe7bc582676437ae336da889081f276c52bd7269b159d37bad2dbc0ec269c9
-size 2628
+oid sha256:0b96855d4b6b6b45614813c164fd67685ed698f7b4d94b6fe018edb968d86e99
+size 2627
