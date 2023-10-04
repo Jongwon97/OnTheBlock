34e202f8417299fa5cb072a49634ddcf03bb4fd2
gyqls234 <gyqls234@naver.com>
2023-10-04 12:56:43 +0900

    design: mypage design update
    Former-commit-id: df90985dd2494d2a72b3b6e8843c3d05e5055849

--- Frontend/src/components/mypage/MyPageInfoInput.jsx
+++ Frontend/src/components/mypage/MyPageInfoInput.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fd8ab8a74bc56176711a7eacef9af6fa7cb5cd20816376e37ae5d1af6f7f5bd
-size 10857
+oid sha256:7a24dfdfb3ae56d8eedba38f841c67241b4e3571c4814d1ff523ad27ca23e675
+size 11814
--- Frontend/src/components/mypage/MyPageSidebar.jsx
+++ Frontend/src/components/mypage/MyPageSidebar.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5feb72935ec7aa58acf620eedb07df4710feda0d3cfcd6a61ef48803784a404f
-size 5231
+oid sha256:4025c2ec3347d9f6486978669b8c129ff7d7a6d58859ee190e666f4e8e035c74
+size 5389
