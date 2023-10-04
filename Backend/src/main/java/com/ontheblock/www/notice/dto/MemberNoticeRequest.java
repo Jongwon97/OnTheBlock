537dbba5d26a5f02bcb3f9a07d7f2fda096b59da
seojoonhee <seojoonhee10@gmail.com>
2023-10-04 10:37:21 +0900

    feat: 알림 링크랑 css
    Former-commit-id: 709499a4e2bb4aade83f21b6cd6fa4f35c459788

--- Backend/src/main/java/com/ontheblock/www/notice/dto/MemberNoticeRequest.java
+++ Backend/src/main/java/com/ontheblock/www/notice/dto/MemberNoticeRequest.java
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9c4be3e66c836425a4060ee27796cd90413a131bb6e925f066c8c0ec656d59c4
-size 165
+package com.ontheblock.www.notice.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberNoticeRequest {
+	private Integer noticeType;
+	private String noticeContent;
+
+}
