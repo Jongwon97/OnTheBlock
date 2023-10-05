be148bd2b864b519eefb16e47aa477af4385c119
seojoonhee <seojoonhee10@gmail.com>
2023-10-05 13:55:36 +0900

    fix: 컴포넌트 구성 변경
    Former-commit-id: 3c9611f43a734a1bc27475d994e212c30dc4948a

--- Frontend/src/components/mypage/MyPageInfoInput.jsx
+++ Frontend/src/components/mypage/MyPageInfoInput.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a24dfdfb3ae56d8eedba38f841c67241b4e3571c4814d1ff523ad27ca23e675
-size 11814
+oid sha256:2374b1e906cdd5187b04618426be5ddb69b8aad65dc2b0fab276e98164e678aa
+size 11672
