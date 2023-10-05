221ce3b7512b223196498f9f14fa02d70f71f108
seojoonhee <seojoonhee10@gmail.com>
2023-10-05 14:54:41 +0900

    feat: video 모달 profile 이동 추가
    Former-commit-id: 3101ab36b17371590076a1f662df12a565fcb64d

--- Frontend/src/components/video/VideoModal.jsx
+++ Frontend/src/components/video/VideoModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c97ff4fe07153f8c407aa653e8f446c3b80dd07a48fcdd6834bfc26822c038f
-size 15189
+oid sha256:3759cb8e80034ac8d987015fe03333434ddf02618cc6d4b4ee1f494db28dcfd4
+size 15614
