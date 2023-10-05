db887cfa9b2ff059e14810c08e4a6df3eca8c9d1
조태규 <hey03312@gmail.com>
2023-10-05 22:59:50 +0900

    feat: elastic search 구현
    Former-commit-id: d85a00e6073d926eebd473b4d5945c2377b3cd64

--- Recommend/ElasticUtil.py
+++ Recommend/ElasticUtil.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af95ecb42ab1e5656281aa5c8285e958e39ed21ae8546e6af578e5c091b37dac
-size 4662
+oid sha256:b4aa64d77c5159578c03f71c842273c7bebd86e9224002bc124e93c666f4646b
+size 4585
