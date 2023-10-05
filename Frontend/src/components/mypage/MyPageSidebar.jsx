768cc90c4733b4fe8c6c8a263c6aeda4d2a05dd2
seojoonhee <seojoonhee10@gmail.com>
2023-10-05 16:02:46 +0900

    design: 한칸 띄기..
    Former-commit-id: a6b6f93878185588cb50e4d4bdf59afd35967623

--- Frontend/src/components/mypage/MyPageSidebar.jsx
+++ Frontend/src/components/mypage/MyPageSidebar.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb8927c395b380414ce7d86b278d7803189a01065865282aaf9b587a512247e5
-size 5444
+oid sha256:54a7d355818fe923ba28b92068afc1fb8ba82c9797d4017cc8e190a8081ecdbf
+size 5463
