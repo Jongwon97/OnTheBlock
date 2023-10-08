44b3c885333c73776424e0858a1f5dd6282258ae
chb <gyqls234@gmail.com>
2023-10-09 00:46:52 +0900

    design: video modal design update
    Former-commit-id: 064c2b0bca6b89c0419e66c4ded85b3fd9587120

---
+++ Frontend/src/components/comment/Comment.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9a854083a87dd6922e693898e072f10e5ac75feb95782efc9712a9dcf721d96f
+size 5012
--- Frontend/src/components/index.js
+++ Frontend/src/components/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:557d08647eff242935dcdc7801f3600ef8b18b12f3e512d99b22f76a147c7968
-size 2382
+oid sha256:ba85fa487846f597f0db14f3bbe1994ba181620fff677e810d5cb9cd50530624
+size 2434
