7634acaa582e462bf06ceb0638c52b897e2b5302
박종성 <whd1707@gmail.com>
2023-10-08 15:17:14 +0900

    fix: db연결 설정 변경
    Former-commit-id: 33da20944b72bfb373294fc9fb4f01d5ba61455a

--- Recommend/config.py
+++ Recommend/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a11054a43c0ea7ed5f5791dd4a8b76daefa2f42ffc37f55639feaa673ceda0cb
-size 1148
+oid sha256:1eddaddf9b6e5fbd0201d53389af35bc8b36aab07e2fcff2d8ea25fc13d2513f
+size 1163
