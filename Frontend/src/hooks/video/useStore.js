cab4aae8f08fc2deb9dde535cc90a2e3ad947456
chb <gyqls234@gmail.com>
2023-10-08 10:43:06 +0900

    fix: videomodal re-render issue fixed
    Former-commit-id: ac6dab81bfca4b2ce225e67e6eab03c8b546074c

--- Frontend/src/hooks/video/useStore.js
+++ Frontend/src/hooks/video/useStore.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1626ee50870e8516c440c1e6d78fd5a57d46d225900d95af10384f90f60cdde0
-size 2863
+oid sha256:24931d12b27b4af3273bb803a6db8cc5b297b64096dc46ff5e1013ef4fa38845
+size 2701
