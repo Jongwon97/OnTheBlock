27e7540da58f41654752c8880ee99fb20f95f229
조태규 <hey03312@gmail.com>
2023-10-16 13:00:37 +0900

    fix:readme 파일
    Former-commit-id: d86906e0f0c9d9e55de250238fdb074574d8c4a4

--- Recommend/ElasticIndexer.py
+++ Recommend/ElasticIndexer.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:632a45124a6a24c67e64f312f87880d5416ec45cad190890e4a45b9991cd7d22
-size 5976
+oid sha256:5f03b1471b2fa0d6ddf5bbe6307de3b404d241ce3ae03186ac1b952871ecda59
+size 6007
--- Recommend/ElasticUtil.py
+++ Recommend/ElasticUtil.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8647ade07c917b1226cd0983f6edf29a09ced1a7726fc4cc078a508ff93b1c93
-size 4714
+oid sha256:3621ae14b14292c8b37f3df097e224f25f76a22215e6b6c9f319f554a7d18be7
+size 4697
